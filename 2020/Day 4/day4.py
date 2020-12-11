d0777fd2042e7789cc0b5f0968021f18826e9e11
Chxrlie <charliedinh01@gmail.com>
2020-12-11 18:34:54 +0000

    Update day4.py

--- 2020/Day 4/day4.py
+++ 2020/Day 4/day4.py
@@ -2,18 +2,18 @@ import re
 
 passportFile = open("2020\Day 4\input.txt").read().splitlines()
 requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-validPassports = 0
-passportSet = []
 
-for line in passportFile:
-    if line != "":
-        passportSet.append(line)
-    elif all(words in ' '.join(map(str, passportSet)) for words in requiredFields):
-        print("Valid Passport")
-        validPassports += 1
-        passportSet = []
-    else:
-        print("Invalid Passport")
-        passportSet = []
-        
-print(validPassports)
+def part1():
+    validPassports = 0
+    passportSet = []
+    for line in passportFile:
+        if line != "":
+            passportSet.append(line)
+        elif all(words in ' '.join(map(str, passportSet)) for words in requiredFields):
+            validPassports += 1
+            passportSet = []
+        else:
+            passportSet = []
+    return validPassports
+
+print("Part 1, Valid Passports: {}".format(part1()))
